91a6a89aae12bb8a98776b09c0296518e3c86279
Terenceonye <onyeweketerence@gmail.com>
2024-10-28 00:27:04 +0100

    final for cun online mail

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.post("/api/course", async (req, res) => {
     // Define the email content
     const mailOptions = {
         from: `"Course Inquiry" <${process.env.EMAIL_USER}>`,
-        to: "online@claretianuniversity.edu.ng",
+        to: "onyeweketerence@gmail.com",
         subject: `New Course Inquiry: ${subject}`,
         html: template( { name, email, program, subject, message } ),
     };
